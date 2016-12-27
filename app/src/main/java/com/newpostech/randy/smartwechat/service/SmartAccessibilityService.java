2ccdc5acd4ebb0bdd2e9488fd6b141a366533c64
Randy <dddrandy@gmail.com>
2016-12-27 23:43:24 +0800

    register accessibility service

---
+++ app/src/main/java/com/newpostech/randy/smartwechat/service/SmartAccessibilityService.java
@@ -0,0 +1,23 @@
+package com.newpostech.randy.smartwechat.service;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+
+/**
+ * Author: randy(dddrandy@gmail.com)
+ * Date: 2016/12/27
+ * Time: 22:40
+ * Description: accessibility service
+ */
+
+public class SmartAccessibilityService extends AccessibilityService {
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+}
